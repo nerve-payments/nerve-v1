206c8ca95645d340c3334f2d9d9a955d940fd4c1
ShashwatMDas <shashwatmdas@gmail.com>
2021-10-11 12:50:42 +0530

    Added merchant and amount global state

--- app/src/store/reducers/paymentReducer.ts
+++ app/src/store/reducers/paymentReducer.ts
@@ -1,5 +1,7 @@
 import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 export const initialState = {
+    amount: 653,
+    merchant: "Shashwat Football"
 } as any;
 
 const paymentReducer = createSlice({
